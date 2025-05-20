02ee4a62cb3fe0618026f035d868c36491fd21bb
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-05-21 01:20:22 +0300

    Add working team deathmatch for `Clear Sky`

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -2471,8 +2471,17 @@ bool CWeapon::unlimited_ammo()
 			return false;
 	}
 
-	return ((GameID() == eGameIDDeathmatch) && 
-			m_DefaultCartridge.m_flags.test(CCartridge::cfCanBeUnlimited)); 
+	if (EngineExternal().CallOfPripyatMode())
+	{
+		return ((GameID() == eGameIDDeathmatch) &&
+			m_DefaultCartridge.m_flags.test(CCartridge::cfCanBeUnlimited));
+	}
+	else
+	{
+		return ((GameID() != eGameIDArtefactHunt) &&
+			(GameID() != eGameIDCaptureTheArtefact) &&
+			m_DefaultCartridge.m_flags.test(CCartridge::cfCanBeUnlimited));
+	}
 			
 }
 bool CWeapon::infinite_fire()
