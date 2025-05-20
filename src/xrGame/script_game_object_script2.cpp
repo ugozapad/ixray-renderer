85b5b58f11fd3f51676f07761a721170b7a7e23f
Drombeys <dima.zolotov02@gmail.com>
2025-05-21 01:20:25 +0300

    Костыль для сна

--- src/xrGame/script_game_object_script2.cpp
+++ src/xrGame/script_game_object_script2.cpp
@@ -81,6 +81,7 @@ class_<CScriptGameObject> script_register_game_object1(class_<CScriptGameObject>
 		.property("bleeding",				&CScriptGameObject::GetBleeding,		&CScriptGameObject::SetBleeding)
 		.property("thirst",					&CScriptGameObject::GetThirst,			&CScriptGameObject::SetThirst)
 		.property("sleepiness",				&CScriptGameObject::GetSleepiness,		&CScriptGameObject::SetSleepiness)
+		.property("sleep",				&CScriptGameObject::GetSleepiness,		&CScriptGameObject::SetSleepiness)
 
 //		.def("get_bleeding",				&CScriptGameObject::GetBleeding)
 		.def("center",						&CScriptGameObject::Center)
